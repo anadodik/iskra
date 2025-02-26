Adding gradients, fixing DEC, reformatting.

iskra/fem.py
@@ -1,42 +1,97 @@
 # Copyright (c) 2022 - present, Ana Dodik. All rights reserved.
 
+from typing import Literal
+
 import torch
 
-from iskra.topology import reduce_on_subface
+from iskra.geometry.volume import volume_form, volume_form_intrinsic
+from iskra.sparse import diag
+from iskra.topology import face_index, reduce_on_subface
 
 
-def mass_matrix(vertex_areas: torch.Tensor) -> torch.Tensor:
-    if vertex_areas.ndim > 1 and vertex_areas.shape[-1] == 1:
-        vertex_areas = vertex_areas.squeeze(-1)
-    n_vertices = vertex_areas.shape[0]
-    ij = torch.arange(n_vertices, device=vertex_areas.device)[None, :].expand(2, -1)
-    result = torch.sparse_coo_tensor(ij, vertex_areas, size=[n_vertices, n_vertices])
-    return result.coalesce()
+def mass(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
+    embedded_faces = face_index(vertices, faces)
+    volume = volume_form(embedded_faces)
+    n_corners = faces.shape[-1]
+    dual_volume = reduce_on_subface(volume / n_corners, faces, vertices.shape[0], "sum")
+    return diag(dual_volume)
 
 
-def mass_matrix_inv(vertex_areas: torch.Tensor) -> torch.Tensor:
-    if vertex_areas.ndim > 1 and vertex_areas.shape[-1] == 1:
-        vertex_areas = vertex_areas.squeeze(-1)
-    n_vertices = vertex_areas.shape[0]
-    ij = torch.arange(n_vertices, device=vertex_areas.device)[None, :].expand(2, -1)
-    result = torch.sparse_coo_tensor(ij, vertex_areas, size=[n_vertices, n_vertices])
-    return result.coalesce()
+def mass_inv(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
+    embedded_faces = face_index(vertices, faces)
+    volume = volume_form(embedded_faces)
+    n_corners = faces.shape[-1]
+    dual_volume = reduce_on_subface(volume / n_corners, faces, vertices.shape[0], "sum")
+    return diag(1 / dual_volume)
 
 
-def mass_matrix_intrinsic(
+def mass_intrinsic(
+    edge_lengths: torch.Tensor,
+    faces: torch.Tensor,
+    face_to_edge: torch.Tensor,
+    n_vertices: int,
+) -> torch.Tensor:
+    area = volume_form_intrinsic(edge_lengths, face_to_edge)
+    n_corners = faces.shape[-1]
+    vertex_areas = reduce_on_subface(area / n_corners, faces, n_vertices, "sum")
+    return diag(vertex_areas)
+
+
+def mass_intrinsic_inv(
+    edge_lengths: torch.Tensor,
+    faces: torch.Tensor,
+    face_to_edge: torch.Tensor,
+    n_vertices: int,
+) -> torch.Tensor:
+    area = volume_form_intrinsic(edge_lengths, face_to_edge)
+    n_corners = faces.shape[-1]
+    vertex_areas = reduce_on_subface(area / n_corners, faces, n_vertices, "sum")
+    return diag(1 / vertex_areas)
+
+
+def grad(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
+    if faces.shape[-1] != 3:
+        raise ValueError("Grad implemented only for triangle meshes.")
+    # TODO(anadodik): make it into a batched [3, n_faces, n_vertices] tensor.
+    n_vertices = vertices.shape[0]
+    n_faces = faces.shape[0]
+    device = vertices.device
+
+    triangles = face_index(vertices, faces)
+    edge_21 = triangles[:, 2, :] - triangles[:, 0, :]
+    edge_13 = triangles[:, 1, :] - triangles[:, 0, :]
+
+    face_normals = torch.linalg.cross(edge_21, -edge_13)
+    double_face_areas = torch.linalg.vector_norm(face_normals, dim=-1, keepdim=True)
+    face_normals = torch.nn.functional.normalize(face_normals, p=2, dim=-1)
+
+    rot_edge_21 = torch.linalg.cross(face_normals, edge_21) / double_face_areas
+    rot_edge_13 = torch.linalg.cross(face_normals, edge_13) / double_face_areas
+
+    idx_i = torch.cat([torch.arange(0, n_faces, device=device)] * 4)
+    idx_j = torch.cat([faces[:, 1], faces[:, 0], faces[:, 2], faces[:, 0]])
+    idx = torch.stack([idx_i, idx_j])
+    values = torch.cat([rot_edge_13, -rot_edge_13, rot_edge_21, -rot_edge_21])
+
+    grad_x = torch.sparse_coo_tensor(idx, values[:, 0], size=[n_faces, n_vertices])
+    grad_y = torch.sparse_coo_tensor(idx, values[:, 1], size=[n_faces, n_vertices])
+    grad_z = torch.sparse_coo_tensor(idx, values[:, 2], size=[n_faces, n_vertices])
+    return grad_x, grad_y, grad_z
+
+
+def laplacian(
+    vertices: torch.Tensor,
+    faces: torch.Tensor,
+    weight_type: Literal["cotan", "uniform"] = "cotan",
+) -> torch.Tensor:
+    pass
+
+
+def laplacian_intrinsic(
     edge_lengths: torch.Tensor,
     face_to_edge: torch.Tensor,
-    faces: torch.Tensor,
     n_vertices: int,
-    inverse: bool = False,
+    faces: torch.Tensor,
+    weight_type: Literal["cotan", "uniform"] = "cotan",
 ) -> torch.Tensor:
-    # TODO(anadodik): refactor
-    face_edge_lengths = edge_lengths[face_to_edge.flatten()].reshape(
-        *face_to_edge.shape, *edge_lengths.shape[1:]
-    )
-    semiperimeters = 0.5 * face_edge_lengths.sum(-1, keepdim=True)
-    area = torch.sqrt(
-        semiperimeters[:, 0] * torch.prod(semiperimeters - face_edge_lengths, dim=-1)
-    )
-    vertex_areas = reduce_on_subface(area, faces, n_vertices, "sum") / 3
-    return mass_matrix(vertex_areas, inverse=inverse)
+    pass

iskra/mesh.py
@@ -17,18 +17,13 @@ import torch
 from iskra.geometry import BBox, triangle_area_normals, triangle_areas
 from iskra.io import load
 from iskra.logging.logging import getLogger
-from iskra.topology import (
-    boundary,
-    face_index,
-    face_scatter_reduce,
-    get_subfaces,
-)
+from iskra.topology import boundary, face_index, face_scatter_reduce, get_subfaces
 
 LOGGER = getLogger(__name__)
 LOGGER.setLevel("INFO")
 
 
-class MeshTopology(torch.nn.Module):
+class Topology(torch.nn.Module):
     def __init__(
         self,
         faces: torch.Tensor,
@@ -72,7 +67,7 @@ class MeshTopology(torch.nn.Module):
         elif dim > self.intrinsic_dim:
             return torch.zeros([0, dim + 1], device=self.faces.device, dtype=torch.long)
 
-        subfaces = subfaces(self.faces, dim)
+        subfaces = get_subfaces(self.faces, dim)
         subfaces = torch.flatten(subfaces, -3, -2)
         subfaces = torch.sort(subfaces, -1)[0]
         subfaces = torch.unique(subfaces, dim=-2)
@@ -86,7 +81,7 @@ class MeshTopology(torch.nn.Module):
         if subface_dim < 0:
             subface_dim = self.intrinsic_dim + subface_dim
 
-        subfaces = subfaces(faces, subface_dim)
+        subfaces = get_subfaces(faces, subface_dim)
         n_subfaces = subfaces.shape[-2]
         subfaces = torch.flatten(subfaces, -3, -2)
         subfaces = torch.sort(subfaces, -1)[0]
@@ -114,8 +109,8 @@ class MeshTopology(torch.nn.Module):
         return self.vertices[mask]
 
 
-class MeshGeometry(torch.nn.Module):
-    def __init__(self, topology: MeshTopology, vertices: torch.Tensor) -> None:
+class Geometry(torch.nn.Module):
+    def __init__(self, topology: Topology, vertices: torch.Tensor) -> None:
         super().__init__()
 
         self.topology = topology
@@ -148,8 +143,6 @@ class MeshGeometry(torch.nn.Module):
     def __getitem__(self, faces: torch.Tensor) -> torch.Tensor:
         if isinstance(faces, torch.Tensor):
             return face_index(self.vertices, faces)
-        elif isinstance(faces, Index):
-            return faces.index_into(self.vertices, 0, self.topology)
         else:
             return self.vertices[faces]
 
@@ -240,18 +233,18 @@ class MeshGeometry(torch.nn.Module):
 class Mesh(torch.nn.Module):
     def __init__(
         self,
-        topology: torch.Tensor | MeshTopology,
-        geometry: torch.Tensor | MeshGeometry,
+        topology: torch.Tensor | Topology,
+        geometry: torch.Tensor | Geometry,
     ) -> None:
         super().__init__()
         if isinstance(topology, torch.Tensor):
-            topology = MeshTopology(topology)
+            topology = Topology(topology)
         if isinstance(geometry, torch.Tensor):
-            geometry = MeshGeometry(topology, geometry)
+            geometry = Geometry(topology, geometry)
         self.topo = topology
         self.geom = geometry
 
-    def __iter__(self) -> Iterator[MeshTopology | MeshGeometry]:
+    def __iter__(self) -> Iterator[Topology | Geometry]:
         return iter([self.topo, self.geom])
 
     @property
@@ -311,8 +304,8 @@ class Mesh(torch.nn.Module):
         #   inv_idx.scatter_(0, vertex_idcs, new_vertex_idcs)
         #   faces = torch.gather(inv_idx, 0, edges.flatten()).reshape(-1, 2)
 
-        topology = MeshTopology(faces)
-        geometry = MeshGeometry(topology, vertices)
+        topology = Topology(faces)
+        geometry = Geometry(topology, vertices)
         mesh = Mesh(topology, geometry)
 
         if return_index:
@@ -330,8 +323,8 @@ class Mesh(torch.nn.Module):
         return_material_ids: bool = False,
     ) -> Self | tuple[Self, torch.Tensor]:
         faces, vertices, uvs, normals, material_ids = load(str(path), device=device)
-        topology = MeshTopology(faces, vertices.shape[0])
-        geometry = MeshGeometry(topology, vertices)
+        topology = Topology(faces, vertices.shape[0])
+        geometry = Geometry(topology, vertices)
 
         mesh = Mesh(topology, geometry)
         if normals is not None and normals.shape[0] == mesh.geom.n_vertices:

iskra/sparse.py
@@ -44,11 +44,11 @@ def torch_to_scipy(x: torch.Tensor) -> torch.Tensor:
     x_scipy = scipy.sparse.coo_array((data, (idcs[0], idcs[1])), shape=x.shape)
     return x_scipy
 
+
 _INDEX_TYPE = slice | int | torch.Tensor | tuple[int, ...]
 
-def _build_index_selection_mask(
-    x: torch.Tensor, *indices: _INDEX_TYPE
-) -> torch.Tensor:
+
+def _build_index_selection_mask(x: torch.Tensor, *indices: _INDEX_TYPE) -> torch.Tensor:
     assert x.layout == torch.sparse_coo
     new_shape = list(x.shape)
     mask = torch.ones(x.indices().shape[-1], device=x.device, dtype=torch.bool)
@@ -87,7 +87,7 @@ def get_slice(x: torch.Tensor, *indices: _INDEX_TYPE) -> torch.Tensor:
     mask, new_shape = _build_index_selection_mask(x, *indices)
     selected_idx = x._indices()[:, mask]
     selected_val = x._values()[mask]
-    
+
     for dim, idx in enumerate(indices):
         match idx:
             case None:
@@ -98,7 +98,7 @@ def get_slice(x: torch.Tensor, *indices: _INDEX_TYPE) -> torch.Tensor:
                 idx_map = torch.empty(x.shape[dim], dtype=torch.long, device=x.device)
                 idx_map[idx] = torch.arange(new_shape[dim], device=idx.device)
                 selected_idx[dim, :] = idx_map[selected_idx[dim, :]]
-    
+
     return torch.sparse_coo_tensor(
         selected_idx,
         selected_val,
@@ -204,6 +204,8 @@ def min_quadratic_energy(
             "rhs must have the same number of dim and same last dim as known_values. "
             f"rhs.shape = {rhs.shape}, known_values.shape = {known_values.shape}."
         )
+    if not system.is_coalesced():
+        system = system.coalesce()
     n_rows = system.shape[0]
     unknown_mask = torch.ones([n_rows], dtype=torch.bool, device=known_idx.device)
     unknown_mask[known_idx] = False
@@ -212,7 +214,7 @@ def min_quadratic_energy(
     system_uk = get_slice(system, unknown_idx, known_idx)
     rhs_u = rhs[unknown_idx, ...]
     new_rhs = rhs_u - system_uk @ known_values
-    
+
     solver = CholeskySolver(system_uu)
     unknown = solver.solve(new_rhs)
     result = torch.zeros_like(rhs)