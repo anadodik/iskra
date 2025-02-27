83946934cea05ce29cf08a5c1ca1c50c2c617fd1
Ana Dodik <anadodik@mit.edu>
2025-02-27 17:49:19 -0500

    [INFRA] Adding RapidOBJ mesh loading.

---
+++ tests/test_io.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2023 - present, Ana Dodik. All rights reserved.
+
+import numpy as np
+import pytest
+import torch
+
+from iskra.io.io import _load_io_ext
+
+
+def test_load_obj() -> None:
+    # TODO:
+    # * test loading triangle mesh
+    # * test loading quad mesh
+    # * test loading point cloud
+    # * test loading multiple shape mesh (should raise error)
+    # * test loading from filesystem and from stream
+    # * test loading uvs, normals, material_ids
+    pass
