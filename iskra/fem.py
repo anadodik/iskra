0fe845b4c1aff719fad45a8390f00e8fcff11b27
ahmed <a.has.mahmoud@gmail.com>
2025-03-26 20:33:57 -0400

    grad in 2d

--- iskra/fem.py
+++ iskra/fem.py
@@ -4,7 +4,7 @@ from typing import Literal
 
 import torch
 
-from iskra.geometry.volume import edge_lengths, volume_form, volume_form_intrinsic
+from iskra.geometry.volume import volume_form, volume_form_intrinsic
 from iskra.sparse import diag
 from iskra.topology import face_index, reduce_on_subface
 
@@ -49,20 +49,7 @@ def mass_intrinsic_inv(
     return diag(1 / vertex_areas)
 
 
-def grad_triangle_3d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
-    """Finite element gradient matrix for 3d triangles.
-
-    Given a triangle mesh in 3d, computes the finite element gradient matrix
-    assuming piecewise linear hat function basis.
-
-    Args:
-    vertices (torch.Tensor):(n,d) tensor vertex list a triangle mesh
-    faces (torch.Tensor): tensor of ints of shape (m, 3) interpreted as face index
-    list of a triangle mesh
-
-    Returns:
-    torch.Tensor: three (m, n) sparse coo tensor of the finite element gradient matrix
-    """
+def grad_3d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
     if faces.shape[-1] != 3:
         raise ValueError("grad_3d() implemented only for triangle meshes.")
     if vertices.shape[-1] != 3:
@@ -73,25 +60,20 @@ def grad_triangle_3d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tenso
     device = vertices.device
 
     triangles = face_index(vertices, faces)
-<<<<<<< HEAD
-    edge_01 = triangles[:, 1, :] - triangles[:, 0, :]
-    edge_20 = triangles[:, 0, :] - triangles[:, 2, :]
-=======
     edge_21 = triangles[:, 1, :] - triangles[:, 0, :]
     edge_13 = triangles[:, 0, :] - triangles[:, 2, :]
->>>>>>> f51c916 (fix grad function. now passing the unit test on 3d triangle)
 
-    face_normals = torch.linalg.cross(edge_01, -edge_20)
+    face_normals = torch.linalg.cross(edge_21, -edge_13)
     double_face_areas = torch.linalg.vector_norm(face_normals, dim=-1, keepdim=True)
     face_normals = torch.nn.functional.normalize(face_normals, p=2, dim=-1)
 
-    rot_edge_01 = torch.linalg.cross(face_normals, edge_01) / double_face_areas
-    rot_edge_20 = torch.linalg.cross(face_normals, edge_20) / double_face_areas
+    rot_edge_21 = torch.linalg.cross(face_normals, edge_21) / double_face_areas
+    rot_edge_13 = torch.linalg.cross(face_normals, edge_13) / double_face_areas
 
     idx_i = torch.cat([torch.arange(0, n_faces, device=device)] * 4)
     idx_j = torch.cat([faces[:, 1], faces[:, 0], faces[:, 2], faces[:, 0]])
     idx = torch.stack([idx_i, idx_j])
-    values = torch.cat([rot_edge_20, -rot_edge_20, rot_edge_01, -rot_edge_01])
+    values = torch.cat([rot_edge_13, -rot_edge_13, rot_edge_21, -rot_edge_21])
 
     grad_x = torch.sparse_coo_tensor(idx, values[:, 0], size=[n_faces, n_vertices])
     grad_y = torch.sparse_coo_tensor(idx, values[:, 1], size=[n_faces, n_vertices])
@@ -99,23 +81,7 @@ def grad_triangle_3d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tenso
     return grad_x, grad_y, grad_z
 
 
-def grad_triangle_2d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
-    """Finite element gradient matrix for 2d triangles.
-
-    Given a triangle mesh in 2d, computes the finite element gradient matrix
-    assuming piecewise linear hat function basis.
-
-    Args:
-    vertices (torch.Tensor):(n,d) tensor vertex list a triangle mesh
-    faces (torch.Tensor): tensor of ints of shape (m, 2) interpreted as face index
-    list of a triangle mesh
-
-    Returns:
-    torch.Tensor: two (m, n) sparse coo tensor of the finite element gradient matrix
-
-    Notes:
-    Taken from https://github.com/sgsellan/gpytoolbox/blob/main/src/gpytoolbox/grad.py
-    """
+def grad_2d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
     if faces.shape[-1] != 3:
         raise ValueError("grad_2d() implemented only for triangle meshes.")
     if vertices.shape[-1] != 2:
@@ -155,69 +121,11 @@ def grad_triangle_2d(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tenso
     return grad_x, grad_y
 
 
-def grad_edges(vertices: torch.Tensor, edges: torch.Tensor) -> torch.Tensor:
-    """Finite element gradient matrix for edges.
-
-    Given a polyline, computes the finite element gradient matrix assuming piecewise
-    linear hat function basis.
-
-    Args:
-    vertices (torch.Tensor): (n,d) tensor vertex list of a polyline where
-    edges (torch.Tensor): tensor of ints of shape (m, 2) interpreted as edge
-    index list of a polyline
-
-    Returns:
-    torch.Tensor: (m, n) sparse coo tensor of the finite element gradient matrix
-    """
-    if edges.shape[-1] != 2:
-        raise ValueError("grad_1d() implemented only for edges only.")
-
-    edge_len = edge_lengths(
-        torch.stack((vertices[edges[:, 1], :], vertices[edges[:, 0], :]), dim=1)
-    )
-
-    edge_len[edge_len == 0] = 1e-8
-
-    inv_len = 1.0 / edge_len
-
-    n_vertices = vertices.shape[0]
-    n_faces = edges.shape[0]
-    device = vertices.device
-
-    idx_i = torch.arange(edges.shape[0], dtype=torch.long, device=device)
-    idx_i = torch.cat((idx_i, idx_i))
-    idx_j = torch.cat((edges[:, 0], edges[:, 1]))
-    idx = torch.stack([idx_i, idx_j])
-
-    values = torch.cat([-inv_len, inv_len])
-
-    grad = torch.sparse_coo_tensor(idx, values, size=[n_faces, n_vertices])
-
-    return grad
-
-
 def grad(vertices: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
-    """Finite element gradient matrix.
-
-    Given a triangle mesh or a polyline, computes the finite element gradient matrix
-    assuming piecewise linear hat function basis.
-
-    Args:
-    vertices (torch.Tensor):(n,d) tensor vertex list of a polyline or triangle mesh
-    faces (torch.Tensor): tensor of ints
-        if (m, 2),  interpret as edge index list of a polyline
-        if (m, 3),  interpret as face index list of a triangle mesh
-
-    Returns:
-    torch.Tensor: S (m, n) sparse coo tensor of the finite element gradient matrix
-    where S is 1 for polyline, 2 for triangle mesh in 2d, and 3 for triangle mesh in 3d
-    """
-    if faces.shape[-1] == 2:
-        return grad_edges(vertices, faces)
     if vertices.shape[-1] == 2:
-        return grad_triangle_2d(vertices, faces)
+        return grad_2d(vertices, faces)
     elif vertices.shape[-1] == 3:
-        return grad_triangle_3d(vertices, faces)
+        return grad_3d(vertices, faces)
 
 
 def laplacian(
