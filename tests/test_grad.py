e3cd0cd7c7639f5f24c1f704e596ce65185d731c
ahmed <a.has.mahmoud@gmail.com>
2025-03-27 10:31:21 -0400

    fix grad 1d test

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -1,47 +1,47 @@
+import numpy as np
 import pytest
 import torch
 
 from iskra.fem import grad
 
-# Taken from https://github.com/sgsellan/gpytoolbox/blob/main/test/test_grad.py
-
 
 def test_grad_polyline() -> None:
-    V = torch.tensor([[0], [0.2], [0.5], [0.98], [1.0]])
-    E = torch.stack(
+    vertices = torch.tensor([[0], [0.2], [0.5], [0.98], [1.0]])
+
+    edges = torch.stack(
         (
-            torch.arange(V.shape[0] - 1, dtype=torch.long),
-            torch.arange(1, V.shape[0], dtype=torch.long),
+            torch.arange(vertices.shape[0] - 1, dtype=torch.long),
+            torch.arange(1, vertices.shape[0], dtype=torch.long),
         ),
         dim=1,
     )
 
-    fun_zero_grad = torch.zeros_like(V) + 5
-    fun_const_grad = 2 * V
-    fun_other_grad = V**2
-    G = grad(V, E)
+    fun_zero_grad = torch.zeros_like(vertices) + 5
 
-    G_dense = torch.tensor(G.todense(order="C"), dtype=V.dtype)
+    fun_const_grad = 2 * vertices
+    fun_other_grad = vertices**2
 
+    g = grad(vertices, edges)
+
+    const_res = g @ fun_zero_grad
     torch.testing.assert_close(
-        G_dense @ fun_zero_grad,
-        torch.zeros_like(G_dense @ fun_zero_grad),
+        const_res,
+        torch.zeros_like(const_res),
         rtol=0,
-        atol=0,
+        atol=1e-5,
     )
 
+    fun_const_res = g @ fun_const_grad
     torch.testing.assert_close(
-        G_dense @ fun_const_grad,
-        torch.full_like(G_dense @ fun_const_grad, 2.0),
+        fun_const_res,
+        torch.full_like(fun_const_res, 2.0),
         rtol=0,
-        atol=1e-6,
+        atol=1e-5,
     )
 
-    edge_centers = (V[:-1, :] + V[1:, :]) / 2.0
-
-    torch.testing.assert_close(
-        G_dense @ fun_other_grad, 2.0 * edge_centers, rtol=0, atol=1e-6
-    )
+    edge_centers = 0.5 * (vertices[:-1, :] + vertices[1:, :])
+    fun_grad_res = g @ fun_other_grad
+    torch.testing.assert_close(fun_grad_res, 2.0 * edge_centers, rtol=0, atol=1e-5)
 
 
 def test_grad_2d_triangle() -> None:
